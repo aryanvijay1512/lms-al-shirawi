b04b9973e431dd56f0da0ea2748d64f9cfc14170
aryanvijay1512 <aryanvijay1512@gmail.com>
2026-04-22 08:08:44 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,35 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ background: "white", margin: 0 }}>
 
+        {/* TOP BAR WITH LEFT + RIGHT LOGO ONLY */}
+        <header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "10px 20px",
+            borderBottom: "1px solid #ddd",
+          }}
+        >
+          {/* LEFT LOGO */}
+          <img
+            src="/logo-left.png"
+            alt="Left Logo"
+            style={{ height: 45 }}
+          />
+
+          {/* RIGHT LOGO */}
+          <img
+            src="/logo-right.png"
+            alt="Right Logo"
+            style={{ height: 45 }}
+          />
+        </header>
+
+        {children}
+      </body>
+    </html>
+  );
+}
